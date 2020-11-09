36a11f7b48b77a322839d0238a6e6c5a4e163fd7
Laurent <lwieser@capcod.eu>
2020-11-09 07:20:27 +0100

    absttract persopo,n

--- ObjectIntroduction/AbstractPerson.cs
+++ ObjectIntroduction/AbstractPerson.cs
@@ -2,17 +2,6 @@
 
 namespace ObjectIntroduction
 {
-    public class Person
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public DateTime Birthdate { get; set; }
-
-        public string Display()
-        {
-            return Firstname + " " + Lastname;
-        }
-    }
     public abstract class AbstractPerson
     {
         public string Firstname { get; set; }
