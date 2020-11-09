74ac6976676c65c9f9edc8da6403d143e52d96cd
Laurent <lwieser@capcod.eu>
2020-11-09 06:39:17 +0100

    init

---
+++ ObjectIntroduction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectIntroduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
