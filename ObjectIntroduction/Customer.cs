833da7c15c450baab6480fe1e9e16640f40de7e1
Laurent <lwieser@capcod.eu>
2020-11-09 07:16:34 +0100

    mvoe person

--- ObjectIntroduction/Customer.cs
+++ ObjectIntroduction/Customer.cs
@@ -3,17 +3,6 @@ using System.Collections.Generic;
 
 namespace ObjectIntroduction
 {
-    public abstract class Person
-    {
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public DateTime Birthdate { get; set; }
-    }
-    public class Employee : Person
-    {
-        public string BadgeNumber { get; set; }
-    }
-
     public class Customer : Person, ICustomer
     {
         public bool HasConsent { get; set; }
