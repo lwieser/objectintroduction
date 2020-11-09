07dd50981c0929e7794653e708490c3d9e6455e1
Laurent <lwieser@capcod.eu>
2020-11-09 10:14:18 +0100

    polymorph

--- ObjectIntroduction/Product.cs
+++ ObjectIntroduction/Product.cs
@@ -1,7 +1,25 @@
-﻿namespace ObjectIntroduction
+﻿using System.Net;
+
+namespace ObjectIntroduction
 {
     public class Product
     {
         public string SerialNumber { get; set; }
+    }
+
+    public class ProductBuilder
+    {
+        public static Product Build(string serial)
+        {
+            return new Product()
+            {
+                SerialNumber = serial
+            };
+        }
+
+        public static Product Build(long serial)
+        {
+            return Build(serial.ToString());
+        }
     }
 }
