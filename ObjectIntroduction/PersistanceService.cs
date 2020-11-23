de17b43d7858de8f1ab7e2fa592d94c2c52c5959
Laurent <lwieser@capcod.eu>
2020-11-23 18:50:18 +0100

    add and list users

---
+++ ObjectIntroduction/PersistanceService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace ObjectIntroduction
+{
+    public class PersistanceService
+    {
+        private ObjectIntroductionContext _context;
+
+        public PersistanceService()
+        {
+            _context = new ObjectIntroductionContext();
+        }
+
+        public void AddPerson(Person product)
+        {
+            _context.Persons.Add(product);
+            _context.SaveChanges();
+        }
+
+        public List<Person> ListPersons()
+        {
+            return _context.Persons.ToList();
+        }
+    }
+}
--- ObjectIntroduction/Program.cs
+++ ObjectIntroduction/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace ObjectIntroduction
 {
@@ -6,19 +7,41 @@ namespace ObjectIntroduction
     {
         static void Main(string[] args)
         {
-            Customer customer = new Customer();
-            customer.Firstname = "Laurent";
-            customer.Lastname = "Wieser";
+            PersistanceService ps = new PersistanceService();
+            do
+            {
+                ShowMenu();
+                var key = Console.ReadLine();
+                switch (key)
+                {
+                    case "1":
+                        Console.WriteLine("(prénom) (nom)");
+                        var name = Console.ReadLine().Split(" ");
+                        var person = new Person()
+                        {
+                            Firstname = name[0],
+                            Lastname = name[1]
+                        };
+                        ps.AddPerson(person);
+                        break;
+                    case "2":
+                        var persons = ps.ListPersons();
+                        foreach (var person1 in persons)
+                        {
+                            Console.WriteLine($"{person1.Id} {person1.Firstname} {person1.Lastname}");
+                        }
+                        break;
+                }
 
-            CustomerStruct customerStruct = new CustomerStruct();
-            customerStruct.Firstname = "Laurent";
-            customerStruct.Lastname = "Wieser";
+            } while (true);
+        }
 
-            Anonymize(customerStruct);
-            Anonymize(customer);
-
-            Console.WriteLine("Prénom customer " + customer.Display());
-            Console.WriteLine("Prénom customerStruct " + customerStruct.Firstname);
+        private static void ShowMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Choisir une action");
+            Console.WriteLine("1/ Ajouter une personne");
+            Console.WriteLine("2/ Lister les personnes");
         }
 
         public static void Anonymize(Customer customer)
